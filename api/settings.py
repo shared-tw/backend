a5b73abbed867b57a06a3eec6af977d3f369c783
Blake <blake.yang@7learnings.com>
2021-06-11 17:22:52 +0200

    add CORS header

--- api/settings.py
+++ api/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_spectacular",
+    "corsheaders",
     "share",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -151,3 +153,7 @@ SPECTACULAR_SETTINGS = {
     # See: https://drf-spectacular.readthedocs.io/en/latest/client_generation.html?highlight=enum#component-issues
     "COMPONENT_SPLIT_REQUEST": True,
 }
+
+# django-cors-headers settings
+# FIXME: use the domain!
+CORS_ALLOW_ALL_ORIGINS = True
