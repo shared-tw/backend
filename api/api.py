f8418e2c4bf64baa29f3ee1c461623f8dabdb308
Blake <blake.yang@7learnings.com>
2021-06-13 10:12:19 +0200

    only use func name in operation id

--- api/api.py
+++ api/api.py
@@ -1,10 +1,18 @@
 from ninja import NinjaAPI
+from ninja.operation import Operation
 
 from authenticator.api import JWTAuthBearer
 from authenticator.api import router as authenticator_router
 from share.api import organization_router, public_router, register_rotuer
 
-api = NinjaAPI(title="shared-tw API", version="0.1.0")
+
+class SharedTWApi(NinjaAPI):
+    def get_openapi_operation_id(self, operation: Operation) -> str:
+        name = operation.view_func.__name__
+        return name.replace(".", "_")
+
+
+api = SharedTWApi(title="shared-tw API", version="0.1.0")
 
 api.add_router("", public_router)
 api.add_router("auth", authenticator_router)
